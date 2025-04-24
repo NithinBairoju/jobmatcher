ffba72fee9adc57df888919028bcbedf5680b021
NithinBairoju <nithinbairoju123@gmail.com>
2025-04-24 19:15:41 +0530

    starting the project

---
+++ src/test/java/com/jobmatch/matchjob/MatchjobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobmatch.matchjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchjobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
